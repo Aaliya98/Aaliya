008e39ea146b57f520e817dea3fa54c2eecebac9
Aaliya98 <47175250+Aaliya98@users.noreply.github.com>
2019-02-12 14:09:17 +0530

    Update vc.py

--- vc.py
+++ vc.py
@@ -1,1 +1,6 @@
-
+it = input()
+if(it=='A' or it=='a' or it=='E' or it=='e' or it=='I'
+ or it=='i' or it=='O' or it=='o' or it=='U' or it=='u'):
+    print(it, "is a Vowel")
+else:
+    print(it, "is a Consonant")
